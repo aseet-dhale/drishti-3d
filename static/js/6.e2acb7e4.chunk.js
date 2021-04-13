58010ca211c913c471e5ed838a247873705403e2
aseet-dhale <46392286+aseet-dhale@users.noreply.github.com>
2021-04-13 14:39:51 +0000

    Deploying to gh-pages from @ aseet-dhale/drishti-3d@304c18a784c40e6f0b621f06af78f2a01b623188 🚀

--- static/js/6.e2acb7e4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdrishti-3d"]=this["webpackJsonpdrishti-3d"]||[]).push([[6],{64:function(e,t,r){"use strict";r.r(t),r.d(t,"default",(function(){return c}));var a=r(8),s=r(6),i=r(66),n=r(10);function c(e){var t=Object(s.useRef)(),r=Object(i.a)("/TEST_3.glb"),c=r.nodes,o=r.materials;return Object(n.jsx)("group",Object(a.a)(Object(a.a)({ref:t},e),{},{dispose:null,children:Object(n.jsx)("mesh",{geometry:c.AB_5.geometry,material:o["main pbr.002"],rotation:[0,-Math.PI/2,0]})}))}i.a.preload("/TEST_3.glb")}}]);
-//# sourceMappingURL=6.e2acb7e4.chunk.js.map
